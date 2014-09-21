93f9405d459ffa6a981a76b6f7525bf8c72a6d6b
bonbalonzo <bon.balonzo@gmail.com>
2014-09-22 00:30:48 +0800

    chech HumanFace
    Signed-off-by: bonbalonzo <bon.balonzo@gmail.com>

--- src/com/bonbalonzo/opencv/face/HumanFace.java
+++ src/com/bonbalonzo/opencv/face/HumanFace.java
@@ -6,14 +6,17 @@ import org.opencv.objdetect.CascadeClassifier;
 
 public class HumanFace implements Face {
 
+	@Override
 	public Mat imageFace(String pathImage) {
 		return Highgui.imread(pathImage);
 	}
-
+	
+	@Override
 	public CascadeClassifier faceClassifier(String classifierPath) {
 		return new CascadeClassifier(classifierPath);
 	}
 
+	@Override
 	public void saveImage(Mat image, String savePath) {
 		Highgui.imwrite(savePath, image);
 		System.out.println("Image saved Location: " + savePath);
